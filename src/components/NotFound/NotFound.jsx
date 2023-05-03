6d99bbafe728a42e18e73e1fa9a242e7882a8a9f
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-05-04 05:06:51 +0600

    added img in error page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./NotFound.css";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import error from "../../assets/error.jpg"
 
 const NotFound = () => {
   return (
     <div className="error-part">
       <Container className="error-page">
+        <img className="error-img" src={error} alt="" />
         <h1>Oops!</h1>
         <h3> <span>404</span> PAGE <span>NOT FOUND</span></h3>
         <p>The page you are looking for might have been removed had its name changed of is temporarily unavailable</p>
